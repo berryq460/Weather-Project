b4da9da590652aa9eb92ba1320747523c2449daa
Quiana Berry <berry.quiana@gmail.com>
2020-12-08 10:13:33 -0500

    .env-edit

--- function.js
+++ function.js
@@ -1,11 +1,13 @@
 /*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
+
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
-/*PUT YOUR OWN KEY HERE - THIS MIGHT NOT WORK
+require('dotenv').config();
+/*PUT  KEY HERE - 
 SUBSCRIBE HERE: https://home.openweathermap.org/users/sign_up*/
-const apiKey = "0081ca45c3ce2c2e7cba8734d7100ec2";
+const apiKey = process.env.API_KEY;
 form.addEventListener("submit", e => {
   e.preventDefault();
   let inputVal = input.value;
